cdf0a838b09fca3a5260bf2bafd95cbd819e9a59
PJayRushton <parker.rushton@octanner.com>
2016-03-01 14:25:33 -0700

    Adds event handling for sliders

--- CircularProgressView/ViewController.swift
+++ CircularProgressView/ViewController.swift
@@ -9,17 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var circularProgressView: CircularProgressView!
+    @IBOutlet weak var progressSlider: UISlider!
+    @IBOutlet weak var widthSlider: UISlider!
+    
+    var goal = Goal(type: .Weight, target: 140)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        goal.weighIns = [WeighIn(weight: 150), WeighIn(weight: 148), WeighIn(weight: 143)]
+        progressSlider.maximumValue = Float(goal.targetLoss!)
+        progressSlider.setValue(goal.progress, animated: false)
+        widthSlider.maximumValue = Float(circularProgressView.frame.size.width / 2)
+        widthSlider.setValue(Float(circularProgressView.lineWidth), animated: false)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        circularProgressView.update(goal)
+    }
+    
+    @IBAction func progressSliderValueChanged(sender: UISlider) {
+        let intSliderValue = Int(sender.value)
+        sender.setValue(Float(intSliderValue), animated: true)
+        goal.weighIns[2] = (WeighIn(weight: goal.weighIns.first!.weight - intSliderValue))
+        circularProgressView.update(goal)
     }
 
-
+    @IBAction func widthSliderValueChanged(sender: UISlider) {
+        circularProgressView.lineWidth = CGFloat(sender.value)
+    }
+    
 }
-
